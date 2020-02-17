265f84912cfba1c70cb4db94cbad8001be5f7f3a
dede_20191130 <1044adad@gmail.com>
2020-02-17 22:05:17 +0900

    20200217_2205__ templates/thread/confirm_topic.html         |  48 ++++++++++++++++++++++++  templates/thread/create_topic.html          |   2 +-  thread_app/__pycache__/urls.cpython-38.pyc  | Bin 405 -> 412 bytes  thread_app/__pycache__/views.cpython-38.pyc | Bin 1054 -> 1408 bytes  thread_app/urls.py                          |   2 +-  thread_app/views.py                         |  55 +++++++++++++++++++++-------  6 files changed, 91 insertions(+), 16 deletions(-)

--- thread_app/views.py
+++ thread_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import DetailView
+from django.views.generic import DetailView, FormView, CreateView
 
 from thread_app.forms import TopicCreateForm
 from thread_app.models import Topic
@@ -12,18 +12,45 @@ class TopicDetailView(DetailView):
     context_object_name = 'topic'
 
 
-def topic_create(request):
+class TopicCreateView(CreateView):
     template_name = 'thread/create_topic.html'
-    ctx = {}
-    if request.method == 'GET':
-        ctx['form'] = TopicCreateForm()
-        return render(request, template_name, ctx)
+    form_class = TopicCreateForm
+    model = Topic
+    success_url = reverse_lazy('base:top')
 
-    if request.method == 'POST':
-        topic_form = TopicCreateForm(request.POST)
-        if topic_form.is_valid():
-            topic_form.save()
-            return redirect(reverse_lazy('base:top'))
+    def form_valid(self, form):
+        ctx = {'form': form}
+        if self.request.POST.get('next', '') == 'confirm':
+            return render(self.request, 'thread/confirm_topic.html', ctx)
+        if self.request.POST.get('next', '') == 'back':
+            return render(self.request, 'thread/create_topic.html', ctx)
+        if self.request.POST.get('next', '') == 'create':
+            return super().form_valid(form)
         else:
-            ctx['form'] = topic_form
-            return render(request, template_name, ctx)
+            # 正常動作ではここは通らない。エラーページへの遷移でも良い
+            return redirect(reverse_lazy('base:top'))
+
+# class TopicFormView(FormView):
+#     template_name = 'thread/create_topic.html'
+#     form_class = TopicCreateForm
+#     success_url = reverse_lazy('base:top')
+#
+#     def form_valid(self, form):
+#         form.save()
+#         return super().form_valid(form)
+
+# def topic_create(request):
+#     template_name = 'thread/create_topic.html'
+#     ctx = {}
+#     if request.method == 'GET':
+#         ctx['form'] = TopicCreateForm()
+#         return render(request, template_name, ctx)
+#
+#     if request.method == 'POST':
+#         topic_form = TopicCreateForm(request.POST)
+#         if topic_form.is_valid():
+#             topic_form.save()
+#             return redirect(reverse_lazy('base:top'))
+#         else:
+#             ctx['form'] = topic_form
+#             return render(request, template_name, ctx)
