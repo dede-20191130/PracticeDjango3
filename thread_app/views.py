146821357d4689990c7e52b5f93f2cf4285026d2
dede_20191130 <1044adad@gmail.com>
2020-02-17 22:48:39 +0900

    20200217_2248__ templates/thread/category.html              |  39 ++++++++++++++++++++++++++++  templates/thread/detail_topic.html          |   3 ++-  thread_app/__pycache__/urls.cpython-38.pyc  | Bin 412 -> 481 bytes  thread_app/__pycache__/views.cpython-38.pyc | Bin 1408 -> 2119 bytes  thread_app/urls.py                          |   1 +  thread_app/views.py                         |  20 +++++++++++---  6 files changed, 59 insertions(+), 4 deletions(-)

--- thread_app/views.py
+++ thread_app/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import DetailView, FormView, CreateView
+from django.views.generic import DetailView, FormView, CreateView, ListView
 
 from thread_app.forms import TopicCreateForm
-from thread_app.models import Topic
+from thread_app.models import Topic, Category
 
 
 class TopicDetailView(DetailView):
@@ -30,6 +30,7 @@ class TopicCreateView(CreateView):
             # 正常動作ではここは通らない。エラーページへの遷移でも良い
             return redirect(reverse_lazy('base:top'))
 
+
 # class TopicFormView(FormView):
 #     template_name = 'thread/create_topic.html'
 #     form_class = TopicCreateForm
@@ -54,3 +55,16 @@ class TopicCreateView(CreateView):
 #         else:
 #             ctx['form'] = topic_form
 #             return render(request, template_name, ctx)
+
+
+class CategoryView(ListView):
+    template_name = 'thread/category.html'
+    context_object_name = 'topic_list'
+
+    def get_queryset(self):
+        return Topic.objects.filter(category__url_code=self.kwargs['url_code'])
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['category'] = get_object_or_404(Category, url_code=self.kwargs['url_code'])
+        return ctx
