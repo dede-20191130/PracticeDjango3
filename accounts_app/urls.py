2041d45c736518b7dff505d15ad5ae0ddf3b4762
dede_20191130 <1044adad@gmail.com>
2020-03-06 10:27:30 +0900

    20200306_1027__ accounts_app/__pycache__/urls.cpython-38.pyc  | Bin 355 -> 411 bytes  accounts_app/__pycache__/views.cpython-38.pyc | Bin 1291 -> 1819 bytes  accounts_app/urls.py                          |   3 ++-  accounts_app/views.py                         |  13 +++++++++++--  templates/base_app/base.html                  |   2 +-  templates/registration/profile.html           |  27 ++++++++++++++++++++++++++  6 files changed, 41 insertions(+), 4 deletions(-)

--- accounts_app/urls.py
+++ accounts_app/urls.py
@@ -5,5 +5,6 @@ from accounts_app import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('create/', views.UserCreateView.as_view(), name='create')
+    path('create/', views.UserCreateView.as_view(), name='create'),
+    path('profile/', views.UserProfileView.as_view(), name="profile"),
 ]
